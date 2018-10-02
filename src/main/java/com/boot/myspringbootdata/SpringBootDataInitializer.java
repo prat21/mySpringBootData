2fcc35ee355ef338506fe489288ce96830ce085d
prat21 <prmukherjee.21@gmail.com>
2018-10-02 20:17:38 +0530

    NA

--- src/main/java/com/boot/myspringbootdata/SpringBootDataInitializer.java
+++ src/main/java/com/boot/myspringbootdata/SpringBootDataInitializer.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SringBootDataInitializer {
+public class SpringBootDataInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SringBootDataInitializer.class, args);
+		SpringApplication.run(SpringBootDataInitializer.class, args);
 	}
 }
